eaa50d88bbac53761636f9daf63c5ce583fecb8c
Zayan S. Khan <zayan_k@icloud.com>
2018-12-08 14:29:36 +0000

    Initial commit

---
+++ app/src/main/java/com/khan/zayan/lendapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.khan.zayan.lendapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_signup);
+    }
+}
