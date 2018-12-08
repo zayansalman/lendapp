2111bc62433098f1234235c12a7164b326cefc7a
Eduard Anghel <eduard.a.anghel@kcl.ac.uk>
2018-12-08 17:33:28 +0000

    added aws stuff

--- app/src/main/java/com/khan/zayan/lendapp/IDActivity.java
+++ app/src/main/java/com/khan/zayan/lendapp/IDActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.microblink.MicroblinkSDK;
+
 
 public class IDActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_id);
+
+        MicroblinkSDK.setLicenseKey("sRwAAAAWY29tLmtoYW4uemF5YW4ubGVuZGFwcOuU+whudalOF3c/xj0p+PmcLbphBj72OIYLcqtuHwjC5TZihjMWf4+AqH0c0tfsAK7GuWQOjapMLazowj0yURzfmLCoSFL076cVMCP8YlULWVsvE9UA1a7ldgC52lo0m3hifYe/KSl9fok0P1mj3tuHxBlitS6WPS8C66dmN2ZL24t01Q+v++6jE8Yj68oboj9g+Y+gKCBSS0iqFc9mpT5L7J4zlD8njuOmEnCTQZ5EIfPe2qcDUt8h", this);
     }
 }
