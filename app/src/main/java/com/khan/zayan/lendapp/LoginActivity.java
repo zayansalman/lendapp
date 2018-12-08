af1401d6b5680a04f88999004b77a131d30c4fe5
Eduard Anghel <eduard.a.anghel@kcl.ac.uk>
2018-12-08 17:16:34 +0000

    added aws stuff

--- app/src/main/java/com/khan/zayan/lendapp/LoginActivity.java
+++ app/src/main/java/com/khan/zayan/lendapp/LoginActivity.java
@@ -3,8 +3,12 @@ package com.khan.zayan.lendapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,6 +26,14 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
+
     }
 
 
