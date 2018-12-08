8cd3fb1f4c1448047e376c3910193a4a567d61fe
Eduard Anghel <eduard.a.anghel@kcl.ac.uk>
2018-12-08 18:19:13 +0000

    first step

--- app/src/main/java/com/khan/zayan/lendapp/IDActivity.java
+++ app/src/main/java/com/khan/zayan/lendapp/IDActivity.java
@@ -1,19 +1,217 @@
 package com.khan.zayan.lendapp;
 
-import android.app.Activity;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.res.Configuration;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.microblink.MicroblinkSDK;
+import com.microblink.entities.recognizers.Recognizer;
+import com.microblink.entities.recognizers.RecognizerBundle;
+import com.microblink.entities.recognizers.blinkid.mrtd.MrtdRecognizer;
+import com.microblink.recognition.RecognitionSuccessType;
+import com.microblink.view.CameraEventsListener;
+import com.microblink.view.recognition.RecognizerRunnerView;
+import com.microblink.view.recognition.ScanResultListener;
 
 
 public class IDActivity extends AppCompatActivity {
+
+
+    private static final int PERMISSION_CAMERA_REQUEST_CODE = 69;
+    private RecognizerRunnerView mRecognizerRunnerView;
+    private MrtdRecognizer mRecognizer;
+    private RecognizerBundle mRecognizerBundle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_id);
 
         MicroblinkSDK.setLicenseKey("sRwAAAAWY29tLmtoYW4uemF5YW4ubGVuZGFwcOuU+whudalOF3c/xj0p+PmcLbphBj72OIYLcqtuHwjC5TZihjMWf4+AqH0c0tfsAK7GuWQOjapMLazowj0yURzfmLCoSFL076cVMCP8YlULWVsvE9UA1a7ldgC52lo0m3hifYe/KSl9fok0P1mj3tuHxBlitS6WPS8C66dmN2ZL24t01Q+v++6jE8Yj68oboj9g+Y+gKCBSS0iqFc9mpT5L7J4zlD8njuOmEnCTQZ5EIfPe2qcDUt8h", this);
+
+        mRecognizer = new MrtdRecognizer();
+
+        // bundle recognizers into RecognizerBundle
+        mRecognizerBundle = new RecognizerBundle(mRecognizer);
+        // create RecognizerRunnerView
+        mRecognizerRunnerView = new RecognizerRunnerView(this);
+
+        // associate RecognizerBundle with RecognizerRunnerView
+        mRecognizerRunnerView.setRecognizerBundle(mRecognizerBundle);
+
+        // scan result listener will be notified when scanning is complete
+        mRecognizerRunnerView.setScanResultListener(mScanResultListener);
+        // camera events listener will be notified about camera lifecycle and errors
+        mRecognizerRunnerView.setCameraEventsListener(mCameraEventsListener);
+
+        mRecognizerRunnerView.create();
+        setContentView(mRecognizerRunnerView);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // you need to pass all activity's lifecycle methods to RecognizerRunnerView
+        mRecognizerRunnerView.start();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // you need to pass all activity's lifecycle methods to RecognizerRunnerView
+        mRecognizerRunnerView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // you need to pass all activity's lifecycle methods to RecognizerRunnerView
+        mRecognizerRunnerView.pause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // you need to pass all activity's lifecycle methods to RecognizerRunnerView
+        mRecognizerRunnerView.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // you need to pass all activity's lifecycle methods to RecognizerRunnerView
+        mRecognizerRunnerView.destroy();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // you need to pass all activity's lifecycle methods to RecognizerRunnerView
+        mRecognizerRunnerView.changeConfiguration(newConfig);
+    }
+
+
+    private final CameraEventsListener mCameraEventsListener = new CameraEventsListener() {
+        @Override
+        public void onCameraPreviewStarted() {
+            // this method is from CameraEventsListener and will be called when camera preview starts
+        }
+
+        @Override
+        public void onCameraPreviewStopped() {
+            // this method is from CameraEventsListener and will be called when camera preview stops
+        }
+
+        @Override
+        public void onError(Throwable exc) {
+            /**
+             * This method is from CameraEventsListener and will be called when
+             * opening of camera resulted in exception or recognition process
+             * encountered an error. The error details will be given in exc
+             * parameter.
+             */
+        }
+
+        @Override
+        @TargetApi(23)
+        public void onCameraPermissionDenied() {
+            /**
+             * Called in Android 6.0 and newer if camera permission is not given
+             * by user. You should request permission from user to access camera.
+             */
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_CAMERA_REQUEST_CODE);
+            /**
+             * Please note that user might have not given permission to use
+             * camera. In that case, you have to explain to user that without
+             * camera permissions scanning will not work.
+             * For more information about requesting permissions at runtime, check
+             * this article:
+             * https://developer.android.com/training/permissions/requesting.html
+             */
+        }
+
+        @Override
+        public void onAutofocusFailed() {
+            /**
+             * This method is from CameraEventsListener will be called when camera focusing has failed.
+             * Camera manager usually tries different focusing strategies and this method is called when all
+             * those strategies fail to indicate that either object on which camera is being focused is too
+             * close or ambient light conditions are poor.
+             */
+        }
+
+        @Override
+        public void onAutofocusStarted(Rect[] areas) {
+            /**
+             * This method is from CameraEventsListener and will be called when camera focusing has started.
+             * You can utilize this method to draw focusing animation on UI.
+             * Areas parameter is array of rectangles where focus is being measured.
+             * It can be null on devices that do not support fine-grained camera control.
+             */
+        }
+
+        @Override
+        public void onAutofocusStopped(Rect[] areas) {
+            /**
+             * This method is from CameraEventsListener and will be called when camera focusing has stopped.
+             * You can utilize this method to remove focusing animation on UI.
+             * Areas parameter is array of rectangles where focus is being measured.
+             * It can be null on devices that do not support fine-grained camera control.
+             */
+        }
+    };
+
+    private final ScanResultListener mScanResultListener = new ScanResultListener() {
+        @Override
+        public void onScanningDone(@NonNull RecognitionSuccessType recognitionSuccessType) {
+            // this method is from ScanResultListener and will be called when scanning completes
+            // you can obtain scanning result by calling getResult on each
+            // recognizer that you bundled into RecognizerBundle.
+            // for example:
+
+            MrtdRecognizer.Result result = mRecognizer.getResult();
+            if (result.getResultState() == Recognizer.Result.State.Valid) {
+                // result is valid, you can use it however you wish
+            }
+
+            // Note that mRecognizer is stateful object and that as soon as
+            // scanning either resumes or its state is reset
+            // the result object within mRecognizer will be changed. If you
+            // need to create a immutable copy of the result, you can do that
+            // by calling clone() on it, for example:
+
+            MrtdRecognizer.Result immutableCopy = result.clone();
+
+            // After this method ends, scanning will be resumed and recognition
+            // state will be retained. If you want to prevent that, then
+            // you should call:
+            mRecognizerRunnerView.resetRecognitionState();
+            // Note that reseting recognition state will clear internal result
+            // objects of all recognizers that are bundled in RecognizerBundle
+            // associated with RecognizerRunnerView.
+
+            // If you want to pause scanning to prevent receiving recognition
+            // results or mutating result, you should call:
+            mRecognizerRunnerView.pauseScanning();
+            // if scanning is paused at the end of this method, it is guaranteed
+            // that result within mRecognizer will not be mutated, therefore you
+            // can avoid creating a copy as described above
+
+            // After scanning is paused, you will have to resume it with:
+            mRecognizerRunnerView.resumeScanning(true);
+            // boolean in resumeScanning method indicates whether recognition
+            // state should be automatically reset when resuming scanning - this
+            // includes clearing result of mRecognizer
+        }
+    };
+
 }
+
+
+
+
